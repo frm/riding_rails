6b8499bc4e80479effe1eccc55b6673a3f5cd126
Fernando Mendes <devfrmendes@gmail.com>
2014-10-12 18:43:14 +0100

    Removes old user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-
-  private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :name)
-    end
 end
